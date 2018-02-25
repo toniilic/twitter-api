37f50ada99c80097d6bb5fe8f2040e3610112141
blanknote <notwillingcurrently@gmail.com>
2018-02-25 11:24:52 +0100

    added screen_name validation

--- index.php
+++ index.php
@@ -14,6 +14,10 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+
+
+
+
 // Add user form
 echo '<h2>Add a new user</h2>';
 echo '<form action="/" method="post">
@@ -28,34 +32,34 @@ if(isset($_POST["id"])) {
     // If users with such id doesn't exist, save to a file,
     // otherwise echo a message
     if(check_if_such_id_already_exists($_POST["id"])) {
+
         echo 'User with id <strong>' . $_POST["id"] . '</strong> already exists.';
         echo 'Please choose another id.';
+    } if(!check_if_such_screen_name_exists($_POST["name"])) {
+
+        echo 'User with screen name <strong>' . $_POST["name"] . '</strong> doesn\'t exist.';
+        echo 'Please choose another screen name.';
     } else {
         // Save data to text file
         file_put_contents(
             "users.txt","\n".$_POST["id"].','.$_POST["name"],FILE_APPEND
         );
     }
-
-
 }
 
+// Check if such screen name already exists
+function check_if_such_screen_name_exists($screen_name)
+{
+    $connection = initiate_api_connection();
 
-
-// Check if user with such id exists
-function check_if_such_id_already_exists($id) {
-    $file = file("users.txt");
-    $len = count($file);
-    $exists = false;
-    for($i = 0; $i < $len; $i++) {
-        $current_id = explode(',',$file[$i])[0];
-        if($current_id === $id) {
-            $exists = true;
-        }
+    $user = $connection->get("users/lookup", ["screen_name" => $screen_name]);
+    # todo check if too many api requests
+    if(!isset($user->errors)) {
+        return true;
     }
-    return $exists;
-
+    return false;
 }
+
 
 // List users from users.txt
 $file = file("users.txt");
@@ -69,24 +73,12 @@ for($i = 0; $i < $len; $i++) {
         .'<br>';
 }
 
-// gets followers for specific user
-function get_followers($screen_name)
-{
-    // Initiate twitter API
-    $access_token = '967643846363500544-mJnImpw58pv2bHzFHeIn6g6RdeK2omj';
 
-    $access_token_secret = 'HoKL2pCJgfJlOU3GlZ8RO88dBRBWocE8YPZMpoLtQnVtE';
-
-    $connection = new TwitterOAuth('ANsvnDrNA6OzTQu0aq9XcbcTN', 'xfYBSsId7qmQuv4KsZuqT13rFQjGSLLLdBbRp7OGCyIXUDIcTf', $access_token, $access_token_secret);
-
-
-    return $connection->get("followers/list", ["screen_name" => $screen_name]);
-}
 
 // List followers of user
 if(isset($_GET["screen_name"])) {
     echo '<h2>Followers of ' . $_GET["screen_name"] . '</h2>';
-    $followers = get_followers('7sedam7');
+    $followers = get_followers($_GET["screen_name"]);
 
     // check if API limit exceeded
     if(isset($followers->errors[0]->message)) {
@@ -105,6 +97,40 @@ if(isset($_GET["screen_name"])) {
 }
 
 
+// gets followers for specific user
+function get_followers($screen_name)
+{
+    $connection = initiate_api_connection();
 
+    return $connection->get("followers/list", ["screen_name" => $screen_name]);
+}
+
+// Initiate Twitter API connection
+function initiate_api_connection()
+{
+    // Initiate twitter API
+    $access_token = '967643846363500544-mJnImpw58pv2bHzFHeIn6g6RdeK2omj';
+
+    $access_token_secret = 'HoKL2pCJgfJlOU3GlZ8RO88dBRBWocE8YPZMpoLtQnVtE';
+
+    $connection = new TwitterOAuth('ANsvnDrNA6OzTQu0aq9XcbcTN', 'xfYBSsId7qmQuv4KsZuqT13rFQjGSLLLdBbRp7OGCyIXUDIcTf', $access_token, $access_token_secret);
+
+    return $connection;
+}
+
+// Check if user with such id exists
+function check_if_such_id_already_exists($id) {
+    $file = file("users.txt");
+    $len = count($file);
+    $exists = false;
+    for($i = 0; $i < $len; $i++) {
+        $current_id = explode(',',$file[$i])[0];
+        if($current_id === $id) {
+            $exists = true;
+        }
+    }
+    return $exists;
+
+}
 
 
